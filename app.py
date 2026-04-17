f777da2d96f98f8c4d1a7ddd0edfe3495ccf99db
hai380571-star <hai380571@gmail.com>
2026-04-17 16:35:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+from flask import Flask, request, jsonify, render_template
 import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "PsychoSense Running 😏"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
